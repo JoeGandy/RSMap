1d4cb70d23c42593a6940c4360229ad7b08a47d9
Joe Gandy <joeg@adaptive.co.uk>
2019-03-01 18:41:25 +0000

    Started work on adding dungeon maps

---
+++ tile_gen/regen_dungeon_tiles.sh
@@ -0,0 +1,16 @@
+#./gdal2tiles-leaflet/gdal2tiles.py -l -p raster -z 0-9 -w none ../static/map/original/osrs_world_map.png ../static/map/generated
+for filename in ../static/map/dungeons/originals/*/*.png ; do
+	original_filename=$(echo $filename | sed -e "s/\.png$//")
+	convert $original_filename.png $original_filename.jpg
+
+        filename=$(echo $original_filename | sed -e "s/\.\.\/static\/map\/dungeons\/originals\/.*\///")
+	mkdir -p ../static/map/dungeons/generated/$filename
+	./gdal2tiles-leaflet/gdal2tiles.py -l -p raster -z 1-5 -w none $original_filename.jpg ../static/map/dungeons/generated/$filename
+	echo $filename
+done
+
+
+
+
+
+
